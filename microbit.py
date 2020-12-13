c8e6a8a4a2df0667bd1719b315f1e56b5a833bf3
Victor Panayotov <victorpanayotov@gmail.com>
2020-12-13 21:53:38 +0000

    Added Radio module and Image class

--- microbit.py
+++ microbit.py
@@ -1,9 +1,6 @@
 # text from https://microbit-micropython.readthedocs.io/en/latest/microbit_micropython_api.html
 
-from collections.abc import Iterable
-from typing import Union, Literal
-
-digitalValue = Union[Literal[1], Literal[0]]
+from typing import List, Any, overload
 
 def sleep(ms: int):
     """sleep for the given number of milliseconds."""
@@ -17,7 +14,7 @@ def panic(error_code: int):
 def reset():
     """resets the micro:bit."""
 
-class _Button:
+class Button:
     def is_pressed(self) -> bool:
         """returns True or False to indicate if the button is pressed at the time of the method call."""
     def was_pressed(self) -> bool:
@@ -25,10 +22,10 @@ class _Button:
     def get_presses(self) -> int:
         """returns the running total of button presses, and resets this counter to zero"""
 
-button_a = _Button()
-button_b = _Button()
+button_a: Button
+button_b: Button
 
-class _Display:
+class Display:
     def get_pixel(self, x: int, y: int) -> int:
         """Get the brightness the brightness of the pixel (x,y). Brightness can be from 0 (the pixel is off) to 9 (the pixel is at maximum brightness)."""
     def set_pixel(self, x: int, y: int, val: int):
@@ -37,17 +34,17 @@ class _Display:
         """clears the display."""
     def show(self, image, delay: int = 0, wait: bool = True, loop: bool = False, clear: bool = False):
         """shows the image."""
-    def show(self, iterable: Iterable, delay: int = 400, wait: bool = True, loop: bool = False, clear: bool = False):
+    def show(self, iterable: Any, delay: int = 400, wait: bool = True, loop: bool = False, clear: bool = False):
         """shows each image or letter in the iterable, with delay ms. in between each."""
     def scroll(self, string: str, delay: int = 400):
         """scrolls a string across the display (more exciting than display.show for written messages)."""
 
-display = _Display()
+display: Display
 
-class _MicroBitPin:
-    def write_digital(self, value: Union[bool, digitalValue]):
+class MicroBitPin:
+    def write_digital(self, value: int):
         """value can be 0, 1, False, True"""
-    def read_digital(self) -> digitalValue:
+    def read_digital(self) -> int:
         """returns either 1 or 0"""
     def write_analog(self, value: int):
         """value is between 0 and 1023"""
@@ -60,26 +57,135 @@ class _MicroBitPin:
     def is_touched(self) -> bool:
         """"""
 
-pin1 = _MicroBitPin()
-pin2 = _MicroBitPin()
-pin3 = _MicroBitPin()
-pin4 = _MicroBitPin()
-pin5 = _MicroBitPin()
-pin6 = _MicroBitPin()
-pin7 = _MicroBitPin()
-pin8 = _MicroBitPin()
-pin9 = _MicroBitPin()
-pin10 = _MicroBitPin()
-pin11 = _MicroBitPin()
-pin12 = _MicroBitPin()
-pin13 = _MicroBitPin()
-pin14 = _MicroBitPin()
-pin15 = _MicroBitPin()
-pin16 = _MicroBitPin()
-pin19 = _MicroBitPin()
-pin20 = _MicroBitPin()
+pin1: MicroBitPin
+pin2: MicroBitPin
+pin3: MicroBitPin
+pin4: MicroBitPin
+pin5: MicroBitPin
+pin6: MicroBitPin
+pin7: MicroBitPin
+pin8: MicroBitPin
+pin9: MicroBitPin
+pin10: MicroBitPin
+pin11: MicroBitPin
+pin12: MicroBitPin
+pin13: MicroBitPin
+pin14: MicroBitPin
+pin15: MicroBitPin
+pin16: MicroBitPin
+pin19: MicroBitPin
+pin20: MicroBitPin
 
 # to do: Images
+
+class Image:
+    """
+    The Image class is used to create images that can be displayed easily on the device’s LED matrix. Given an image object it’s possible to display it via the display API:
+    
+        display.show(Image.HAPPY)
+    """
+    HEART: 'Image'
+    HEART_SMALL: 'Image'
+    HAPPY: 'Image'
+    SMILE: 'Image'
+    SAD: 'Image'
+    CONFUSED: 'Image'
+    ANGRY: 'Image'
+    ASLEEP: 'Image'
+    SURPRISED: 'Image'
+    SILLY: 'Image'
+    FABULOUS: 'Image'
+    MEH: 'Image'
+    YES: 'Image'
+    NO: 'Image'
+    CLOCK12: 'Image'
+    CLOCK11: 'Image'
+    CLOCK10: 'Image'
+    CLOCK9: 'Image'
+    CLOCK8: 'Image'
+    CLOCK7: 'Image'
+    CLOCK6: 'Image'
+    CLOCK5: 'Image'
+    CLOCK4: 'Image'
+    CLOCK3: 'Image'
+    CLOCK2: 'Image'
+    CLOCK1: 'Image'
+    ARROW_N: 'Image'
+    ARROW_NE: 'Image'
+    ARROW_E: 'Image'
+    ARROW_SE: 'Image'
+    ARROW_S: 'Image'
+    ARROW_SW: 'Image'
+    ARROW_W: 'Image'
+    ARROW_NW: 'Image'
+    TRIANGLE: 'Image'
+    TRIANGLE_LEFT: 'Image'
+    CHESSBOARD: 'Image'
+    DIAMOND: 'Image'
+    DIAMOND_SMALL: 'Image'
+    SQUARE: 'Image'
+    SQUARE_SMALL: 'Image'
+    RABBIT: 'Image'
+    COW: 'Image'
+    MUSIC_CROTCHET: 'Image'
+    MUSIC_QUAVER: 'Image'
+    MUSIC_QUAVERS: 'Image'
+    PITCHFORK: 'Image'
+    XMAS: 'Image'
+    PACMAN: 'Image'
+    TARGET: 'Image'
+    TSHIRT: 'Image'
+    ROLLERSKATE: 'Image'
+    DUCK: 'Image'
+    HOUSE: 'Image'
+    TORTOISE: 'Image'
+    BUTTERFLY: 'Image'
+    STICKFIGURE: 'Image'
+    GHOST: 'Image'
+    SWORD: 'Image'
+    GIRAFFE: 'Image'
+    SKULL: 'Image'
+    UMBRELLA: 'Image'
+    SNAKE: 'Image'
+
+    ALL_CLOCKS: List['Image']
+    ALL_ARROWS: List['Image']
+
+    @overload
+    def __init__(self, string: str) -> None:
+        """If `string` is used, it has to consist of digits 0-9 arranged into lines, describing the image, for example:
+            image = Image("90009:"
+                          "09090:"
+                          "00900:"
+                          "09090:"
+                          "90009")
+        will create a 5×5 image of an X.
+        The end of a line is indicated by a colon.
+        It’s also possible to use a newline (n) to indicate the end of a line like this:
+            image = Image("90009\n"
+                          "09090\n"
+                          "00900\n"
+                          "09090\n"
+                          "90009")
+        """
+
+    @overload
+    def __init__(self, width: int = None, height: int = None, buffer: Any = None) -> None:
+        """Creates an empty image with `width` columns and `height` rows.
+        Optionally `buffer` can be an array of `width * height` integers in range 0-9 to initialize the image:
+            Image(2, 2, b'\x08\x08\x08\x08')
+        or
+            Image(2, 2, bytearray([9,9,9,9]))
+        will create a 2 x 2 pixel image at full brightness.
+        """
+    def width(self) -> int:
+        """Return the number of columns in the image."""
+    def height(self) -> int:
+        """Return the numbers of rows in the image."""
+    def set_pixel(self, x, y, value):
+        """Set the brightness of the pixel at column `x` and row `y` to the value, which has to be between 0 (dark) and 9 (bright).
+        This method will raise an exception when called on any of the built-in read-only images, like `Image.HEART`."""
+
 # to do: The accelerometer
 # to do: The compass
 # to do: I2C bus
---
+++ radio.py
@@ -0,0 +1,146 @@
+# Text taken from https://microbit-micropython.readthedocs.io/en/latest/radio.html
+
+"""The radio module allows devices to work together via simple wireless networks.
+
+The radio module is conceptually very simple:
+
+    - Broadcast messages are of a certain configurable length (up to 251 bytes).
+    - Messages received are read from a queue of configurable size (the larger the queue the more RAM is used). If the queue is full, new messages are ignored. Reading a message removes it from the queue.
+    - Messages are broadcast and received on a preselected channel (numbered 0-83).
+    - Broadcasts are at a certain level of power - more power means more range.
+    - Messages are filtered by address (like a house number) and group (like a named recipient at the specified address).
+    - The rate of throughput can be one of three pre-determined settings.
+    - Send and receive bytes to work with arbitrary data.
+    - Use receive_full to obtain full details about an incoming message: the data, receiving signal strength, and a microsecond timestamp when the message arrived.
+    - As a convenience for children, it’s easy to send and receive messages as strings.
+    - The default configuration is both sensible and compatible with other platforms that target the BBC micro:bit.
+
+Example:
+
+    # A micro:bit Firefly.
+    # By Nicholas H.Tollervey. Released to the public domain.
+    import radio
+    import random
+    from microbit import display, Image, button_a, sleep
+    
+    # Create the "flash" animation frames. Can you work out how it's done?
+    flash = [Image().invert()*(i/9) for i in range(9, -1, -1)]
+    
+    # The radio won't work unless it's switched on.
+    radio.on()
+    
+    # Event loop.
+    while True:
+        # Button A sends a "flash" message.
+        if button_a.was_pressed():
+            radio.send('flash')  # a-ha
+        # Read any incoming messages.
+        incoming = radio.receive()
+        if incoming == 'flash':
+            # If there's an incoming "flash" message display
+            # the firefly flash animation after a random short
+            # pause.
+            sleep(random.randint(50, 350))
+            display.show(flash, delay=100, wait=False)
+            # Randomly re-broadcast the flash message after a
+            # slight delay.
+            if random.randint(0, 9) == 0:
+                sleep(500)
+                radio.send('flash')  # a-ha
+"""
+
+from typing import Any, Tuple
+
+
+RATE_250KBIT = 0
+"""Constant used to indicate a throughput of 256 Kbit a second."""
+RATE_1MBIT = 1
+"""Constant used to indicate a throughput of 1 MBit a second."""
+RATE_2MBIT = 2
+"""Constant used to indicate a throughput of 1 MBit a second."""
+
+def on():
+    """Turns the radio on. This needs to be explicitly called since the radio draws power and takes up memory that you may otherwise need."""
+
+def off():
+    """Turns off the radio, thus saving power and memory."""
+
+def config(**kwargs):
+    """Configures various keyword based settings relating to the radio. The available settings and their sensible default values are listed below.
+
+    The `length` (default=32) defines the maximum length, in bytes, of a message sent via the radio. It can be up to 251 bytes long (254 - 3 bytes for S0, LENGTH and S1 preamble).
+
+    The `queue` (default=3) specifies the number of messages that can be stored on the incoming message queue. If there are no spaces left on the queue for incoming messages, then the incoming message is dropped.
+
+    The `channel` (default=7) can be an integer value from 0 to 83 (inclusive) that defines an arbitrary “channel” to which the radio is tuned.
+        Messages will be sent via this channel and only messages received via this channel will be put onto the incoming message queue.
+        Each step is 1MHz wide, based at 2400MHz.
+
+    The `power` (default=6) is an integer value from 0 to 7 (inclusive) to indicate the strength of signal used when broadcasting a message.
+        The higher the value the stronger the signal, but the more power is consumed by the device.
+        The numbering translates to positions in the following list of dBm (decibel milliwatt) values: -30, -20, -16, -12, -8, -4, 0, 4.
+
+    The `address` (default=0x75626974) is an arbitrary name, expressed as a 32-bit address, that’s used to filter incoming packets at the hardware level, keeping only those that match the address you set.
+        The default used by other micro:bit related platforms is the default setting used here.
+
+    The `group` (default=0) is an 8-bit value (0-255) used with the `address` when filtering messages.
+        Conceptually, “address” is like a house/office address and “group” is like the person at that address to which you want to send your message.
+
+    The `data_rate` (default=radio.RATE_1MBIT) indicates the speed at which data throughput takes place. Can be one of the following contants defined in the `radio` module : `RATE_250KBIT`, `RATE_1MBIT` or `RATE_2MBIT`.
+    
+    If config is not called then the defaults described above are assumed.
+    """
+
+def reset():
+    """Reset the settings to their default values (as listed in the documentation for the `config` function above)."""
+
+def send_bytes(message: bytes):
+    """Sends a message containing bytes."""    
+
+def receive_bytes() -> bytes:
+    """Receive the next incoming message on the message queue. Returns `None` if there are no pending messages.
+    
+    Messages are returned as bytes.
+    """
+
+def receive_bytes_into(buffer: Any) -> int:
+    """Receive the next incoming message on the message queue.
+    
+    Copies the message into `buffer`, trimming the end of the message if necessary.
+    Returns `None` if there are no pending messages, otherwise it returns the length of the message (which might be more than the length of the buffer).
+    """
+
+def send(message: str):
+    """Sends a message string.
+
+    This is the equivalent of `send_bytes(bytes(message, 'utf8'))` but with `b'\x01\x00\x01'` prepended to the front (to make it compatible with other platforms that target the micro:bit).
+    """
+
+def recieve() -> str:
+    """Works in exactly the same way as receive_bytes but returns whatever was sent.
+
+    Currently, it’s equivalent to `str(receive_bytes(), 'utf8')` but with a check that the the first three bytes are `b'\x01\x00\x01'` (to make it compatible with other platforms that may target the micro:bit).
+    It strips the prepended bytes before converting to a string.
+
+    A `ValueError` exception is raised if conversion to string fails.
+    """
+
+def recieve_full() -> Tuple[bytes, int, int]:
+    """Returns a tuple containing three values representing the next incoming message on the message queue. If there are no pending messages then `None` is returned.
+
+    The three values in the tuple represent:
+
+        the next incoming message on the message queue as bytes.
+        the RSSI (signal strength): a value between 0 (strongest) and -255 (weakest) as measured in dBm.
+        a microsecond timestamp: the value returned by `time.ticks_us()` when the message was received.
+
+    For example:
+
+        details = radio.receive_full()
+        if details:
+            msg, rssi, timestamp = details
+
+    This function is useful for providing information needed for triangulation and/or triliteration with other micro:bit devices.
+    """
+
+
